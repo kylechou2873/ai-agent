75fa83a6757149a2410eeb6534c27f3e24acea27
Kyle Chou <kylechou25@gmail.com>
2026-01-16 16:13:04 +0800

    LLM

--- main.py
+++ main.py
@@ -1,5 +1,36 @@
+import os
+import argparse
+from dotenv import load_dotenv
+from google import genai
+from google.genai import types
+
 def main():
-    print("Hello from ai-agent!")
+    parser = argparse.ArgumentParser(description="Chatbot")
+    parser.add_argument("user_prompt", type=str, help="Prompt to send to Gemini")
+    parser.add_argument("--verbose", action="store_true", help="Enable verbose output")
+    args = parser.parse_args()
+
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    if api_key is None:
+        raise RuntimeError("api_key missing")
+    client = genai.Client(api_key=api_key)
+    
+    messages = [types.Content(role="user", parts=[types.Part(text=args.user_prompt)])]
+    response = client.models.generate_content(
+        model = "gemini-2.5-flash",
+        contents = messages
+    )
+    
+    if response.usage_metadata is not None:
+        if args.verbose:
+            print(f"User prompt: {args.user_prompt}")
+            print(f"Prompt tokens: {response.usage_metadata.prompt_token_count }")
+            print(f"Response tokens: {response.usage_metadata.candidates_token_count }")
+    else:
+        raise Exception("usage_metadata missing")
+    print("Response:")
+    print(response.text)
 
 
 if __name__ == "__main__":
